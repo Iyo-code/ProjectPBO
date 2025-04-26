be1f59e2aea5eb5eda6323509e3c7e865c41b7b7
Iyo-code <anjaytrio913@gmail.com>
2025-04-26 23:06:36 +0800

    source main

---
+++ Source/investasi/main/AdminMenu.java
@@ -0,0 +1,25 @@
+package investasi.main;
+
+import investasi.model.Saham;
+import investasi.model.SuratBerhargaNegara;
+import investasi.service.AdminService;
+
+import java.util.ArrayList;
+
+public class AdminMenu {
+    private static ArrayList<Saham> daftarSaham = new ArrayList<>();
+    private static ArrayList<SuratBerhargaNegara> daftarSBN = new ArrayList<>();
+
+    public void show() {
+        AdminService service = new AdminService(daftarSaham, daftarSBN);
+        service.menuAdmin();
+    }
+
+    public static ArrayList<Saham> getDaftarSaham() {
+        return daftarSaham;
+    }
+
+    public static ArrayList<SuratBerhargaNegara> getDaftarSBN() {
+        return daftarSBN;
+    }
+}
